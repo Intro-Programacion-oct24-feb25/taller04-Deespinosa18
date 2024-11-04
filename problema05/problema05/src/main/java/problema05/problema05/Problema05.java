9bffec9e851313f941b7946aa55810a0501c8705
Diego Espinosa <diegoeespinosag18@gmail.com>
2024-11-04 11:30:50 -0500

    ejercicio

---
+++ problema05/problema05/src/main/java/problema05/problema05/Problema05.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package problema05.problema05;
+
+/**
+ *
+ * @author Diego
+ */
+public class Problema05 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
