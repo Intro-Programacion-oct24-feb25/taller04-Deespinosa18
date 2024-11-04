2676151c8cca86813a99f84385a2d988758dddd6
Diego Espinosa <diegoeespinosag18@gmail.com>
2024-11-04 11:32:02 -0500

    ejercicio

--- problema05/problema05/src/main/java/problema05/problema05/Problema05.java
+++ problema05/problema05/src/main/java/problema05/problema05/Problema05.java
@@ -4,6 +4,8 @@
 
 package problema05.problema05;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Diego
@@ -11,6 +13,27 @@ package problema05.problema05;
 public class Problema05 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner entrada= new Scanner (System.in);
+        double netflix, youtube, dropbox, spotify;
+        double desc1, desc2, desc3;
+        
+        System.out.println("Escriba el costo de Netflix:");
+        netflix = entrada.nextDouble();
+        
+        System.out.println("Escriba el costo de Youtube Premium:");
+        youtube = entrada.nextDouble();
+        
+        System.out.println("Escriba el costo de Dropbox:");
+        dropbox = entrada.nextDouble();
+        
+        System.out.println("Escriba el costo de Spotify:");
+        spotify = entrada.nextDouble();
+        
+        desc1= netflix * 0.23;
+        desc2= youtube * 0.23;
+        desc3= dropbox * 0.45;
+        double valorT= desc1 + desc2 + desc3 + spotify;
+        System.out.println("El valor total a cancelar por todo es de:"
+        + valorT );
     }
 }
