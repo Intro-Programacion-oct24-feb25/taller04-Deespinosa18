209bb8193eb8eba6a807f2c5892c23f84c07d086
Diego Espinosa <diegoeespinosag18@gmail.com>
2024-11-03 20:41:08 -0500

    Tercer problema

---
+++ problema03/problema03/src/main/java/problema03/problema03/Problema03.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package problema03.problema03;
+
+/**
+ *
+ * @author Diego
+ */
+public class Problema03 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
